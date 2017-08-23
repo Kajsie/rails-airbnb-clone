f03ec1782c736d528542b832d96cd350ddd21038
kajsa kristersson <k.kristersson@gmail.com>
2017-08-23 13:44:37 +0200

    added depdestroy and capitalize

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_one :hairdresser
+  has_one :hairdresser, dependent: :destroy
   has_many :bookings
   validates_presence_of :first_name, :last_name
 end
