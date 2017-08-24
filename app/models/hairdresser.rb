cb55b8caccc4cfe6b6adc5bb8eb97055bed09d40
Laura Pesce <laurapesce01@gmail.com>
2017-08-24 14:49:51 +0200

    added validation to hairdresser

--- app/models/hairdresser.rb
+++ app/models/hairdresser.rb
@@ -4,6 +4,7 @@ class Hairdresser < ApplicationRecord
   has_many :bookings
   has_many :availabilities
   validates :city, :location, :description, presence: true
+  validates :user, presence: true
 
   geocoded_by :street
   after_validation :geocode, if: :street_changed?
