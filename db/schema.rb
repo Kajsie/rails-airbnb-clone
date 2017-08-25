889bea801199a7e34805ead27f09437252ecf1d1
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-08-25 12:40:50 +0200

    Added review function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825100111) do
+ActiveRecord::Schema.define(version: 20170825102330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,8 +67,10 @@ ActiveRecord::Schema.define(version: 20170825100111) do
 
   create_table "reviews", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "hairdresser_id"
+    t.index ["hairdresser_id"], name: "index_reviews_on_hairdresser_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -94,4 +96,5 @@ ActiveRecord::Schema.define(version: 20170825100111) do
   add_foreign_key "bookings", "hairdressers"
   add_foreign_key "bookings", "users"
   add_foreign_key "hairdressers", "users"
+  add_foreign_key "reviews", "hairdressers"
 end
